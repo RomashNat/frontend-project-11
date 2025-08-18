416d1068cf0b076ed65c10b954ddc8d0abe28028
Nataly <tatasay@mail.ru>
2025-08-18 17:15:24 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
@@ -8,7 +7,7 @@ import 'bootstrap';
 document.querySelector('#app').innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
